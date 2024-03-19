3ea54c053f4ac42fe2d17ed896d5576baa704593
Ayman <ayman.alshehri@collegecanada.com>
2024-03-18 23:53:26 -0400

    Pull the name into a variable

--- website/views.py
+++ website/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html', { "name": "Ayman"})
+    myname = "Ayman"
+    return render(request, 'home.html', { "name": myname})
