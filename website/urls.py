ac9c5111f027a01a059c0d07ca9c28de68be9b95
Ayman <ayman.alshehri@collegecanada.com>
2024-03-19 00:22:09 -0400

    Put default for name

--- website/urls.py
+++ website/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name="home"),
     path('<str:guestname>', views.home, name="home"),
 ]
--- website/views.py
+++ website/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 
 
-def home(request, guestname):
-    myname = "Ayman"
+def home(request, guestname="Ayman"):
     return render(request, 'home.html', { "name": guestname})
