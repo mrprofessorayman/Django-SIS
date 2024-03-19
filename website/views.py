407e740f54f30db6e8ad1d9db037603b437d775b
Ayman <ayman.alshehri@collegecanada.com>
2024-03-19 00:10:35 -0400

    Pass information from the url

--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
 
-def home(request):
+def home(request, guestname):
     myname = "Ayman"
-    return render(request, 'home.html', { "name": myname})
+    return render(request, 'home.html', { "name": guestname})
